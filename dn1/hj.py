e93ac49718d179613da69092d4a79621a73bb4cc
Smotko <ensmotko@gmail.com>
2012-02-21 10:11:23 +0100

    Made the script work

--- dn1/hj.py
+++ dn1/hj.py
@@ -7,16 +7,16 @@ Created on Feb 20, 2012
 import collections
 from matplotlib import pyplot as plt
 
-fname = "minidata/trainingData.csv"
+fname = "trainingData.csv"
 f = file(fname)
 #n = len(f.readlines())
 #f.seek(0)
 #m = len(f.readline().strip().split("\t"))
 #print "%d instances, %d attributes" % n, m
 
-x = [2,5,6]
+x = [2, 5, 6]
 c = collections.Counter(x)
-c.update([2,1])
+c.update([2, 1])
 print c
 
 ipi = [] # items per data instance
@@ -24,7 +24,7 @@ ipi = [] # items per data instance
 f.seek(0)
 c = collections.Counter()
 for line in f:
-    ind = [i for i,x in enumerate(line.strip().split("\t")) if x != "0"]
+    ind = [i for i, x in enumerate(line.strip().split("\t")) if x != "0"]
     ipi.append(len(ind))
     c.update(ind)
     
