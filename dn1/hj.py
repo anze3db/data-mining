b9f20da52317d453a2e7732c4a06177271138241
Smotko <ensmotko@gmail.com>
2012-02-26 22:37:10 +0100

    Finished dn1

--- dn1/hj.py
+++ dn1/hj.py
@@ -3,36 +3,67 @@ Created on Feb 20, 2012
 
 @author: smotko
 '''
-
 import collections
 from matplotlib import pyplot as plt
 
-fname = "trainingData.csv"
+fname = "dn1/trainingData.csv"
 f = file(fname)
-#n = len(f.readlines())
-#f.seek(0)
-#m = len(f.readline().strip().split("\t"))
-#print "%d instances, %d attributes" % n, m
-
-x = [2, 5, 6]
-c = collections.Counter(x)
-c.update([2, 1])
-print c
+n = len(f.readlines()) # primeri
+f.seek(0)
+m = len(f.readline().strip().split("\t")) # atributi
+print "%d instances, %d attributes" % (n, m) # atributi so tipa int
 
 ipi = [] # items per data instance
 
 f.seek(0)
 c = collections.Counter()
+
 for line in f:
     ind = [i for i, x in enumerate(line.strip().split("\t")) if x != "0"]
     ipi.append(len(ind))
     c.update(ind)
     
-print c[1], c[2], c[3]
+print "%f ratio" % (sum(ipi) / float(n * m))
+print "%d of zero attributes" % (m - len(c.keys()))
+print c.most_common(1)
+
+
+tLabels = open('dn1/trainingLabels.csv', 'r').read().strip().split("\n")
+tUnique = set()
+tLabelsCount = []
+labelsCounter = collections.Counter()
+
+for i in xrange(len(tLabels)):
+    tUnique.update(set(tLabels[i].strip().split(",")))
+    labelsCounter.update(tLabels[i].strip().split(","))
+    tLabelsCount.append(len(tLabels[i].strip().split(",")))
+    tLabels.append(tLabels[i].strip().split(","))
+    
+print labelsCounter.most_common(3)
+print max(tLabelsCount)
+print "%d unique labels" % (len(tUnique))
+
 plt.close()
 plt.hist(ipi)
-plt.show()
+plt.xlabel("Stevilo nenicelnih atributov")
+plt.ylabel("Stevilo primerov")
+plt.title("St. nenicelnih atributov za posamezen primer")
+plt.savefig('dn1/report/examples.png')
+plt.close()
 
+plt.hist(c.values(), bins=20, range=(0, 100))
+plt.xlabel("Stevilo nenicelnih vrednosti")
+plt.ylabel("Stevilo atributov")
+plt.title("Stevilo nenicelnih vrednosti")
+plt.savefig('dn1/report/attributes.png')
+plt.close()
+
+plt.hist(tLabelsCount)
+plt.xlabel("Stevilo oznak (razredov)")
+plt.ylabel("Stevilo primerov")
+plt.title("Razlicne oznake v primerih")
+plt.savefig('dn1/report/labels.png')
+plt.close()
 
 
 if __name__ == '__main__':
