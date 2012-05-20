3a901d60e9d6471d00d8d11c402b0d943b8675fe
Smotko <ensmotko@gmail.com>
2012-05-20 18:04:13 +0200

    Added cost function from dn5

--- dn7/dn7.py
+++ dn7/dn7.py
@@ -3,8 +3,11 @@ import scipy.optimize as opt
 def log_reg(X, y, lambda_):
 
     def cost(thetas, X, y, lambda_):
-        pass
-        
+        '''
+        Compute cost for logistic regression
+        '''
+        return sum([(x1*thetas[1]+thetas[0]-y[i])**2 for i,x1 in enumerate(X)])*0.5/len(X)
+            
     def grad(thetas, X, y, lambda_):
         pass
     
